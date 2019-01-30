ccf0fa52d10632d6d1694c55d7f81d65192df7f3
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-30 17:10:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
-    })
+      event.preventDefault(); 
+    });
 }
 
-preventRefreshOnSubmit()
+preventRefreshOnSubmit();
+
+
+function retrieveEmployeeInformation(){
+   let input = document.querySelector('input'); 
+    return input.value;
+}
+
+
+function addNewElementAsLi(){ 
+  let list = document.querySelector('.employee-list'); 
+  let info = retrieveEmployeeInformation(); 
+  list.append(info); 
+}
+
+function addNewLiOnClick(){
+  retrieveEmployeeInformation(); 
+  addNewElementAsLi(); 
+  let input = document.querySelector('input');
+  input.value = ''; 
+}
+
+function clearEmployeeListOnLinkClick() {
+  let link = document.querySelector('a');
+  let ul = document.querySelector('ul');
+  link.addEventListener('click', function(e) {
+    ul.innerHTML = '';
+  });
+}
