35284ae3c3d4130a050d068e105f3c0d4798b7c1
Douglas Uretsky <douglasuretsky@gmail.com>
2019-02-26 13:57:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
+      event.preventDefault();
     })
 }
 
 preventRefreshOnSubmit()
+
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+
+function addNewElementAsLi() {
+  let theUl = document.querySelector(".employee-list")
+  let el = retrieveEmployeeInformation();
+  theUl.append(el);
+}
+
+function addNewLiOnClick() {
+  let theUl = document.querySelector(".employee-list")
+  let el = retrieveEmployeeInformation();
+  document.querySelector('form').addEventListener('click', function(event){
+    addNewElementAsLi()
+    document.querySelector('input').value = '';
+  });
+}
+
+function clearEmployeeListOnLinkClick() {
+  document.querySelector('a').addEventListener('click', function(event){
+    theUl = document.querySelector(".employee-list")
+    theUl.innerHTML = ''
+  });
+}
