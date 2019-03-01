574dde5cb76974c75e281b7ce9f7bf82efeaa3a7
Cheng Vue <csalvue@gmail.com>
2019-03-01 02:02:50 +0000

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,25 @@ function preventRefreshOnSubmit(){
     })
 }
 
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  return document.querySelector('.employee-list').append(retrieveEmployeeInformation())
+}
+
+function addNewLiOnClick() {
+  document.body.addEventListener('click', function(event){
+    document.querySelector('.employee-list').append(retrieveEmployeeInformation())
+    document.querySelector('input').value = '';
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  document.querySelector('a').addEventListener('click', function(event){
+    document.querySelector('ul').innerHTML = '';
+  })
+}
+
 preventRefreshOnSubmit()
