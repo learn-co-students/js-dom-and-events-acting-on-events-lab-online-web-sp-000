af24e6e2fb92539da5629a7b67e89c199cf369f0
aburk3 <aburk3@gmail.com>
2019-04-03 06:20:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
-    })
+      event.preventDefault();
+    });
 }
 
-preventRefreshOnSubmit()
+preventRefreshOnSubmit();
+
+input = document.querySelector('input');
+
+function retrieveEmployeeInformation() {
+  return input.value;
+}
+
+function addNewElementAsLi() {
+  let newEmployee = retrieveEmployeeInformation();
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${newEmployee}</li>`);
+}
+
+function addNewLiOnClick(){
+  let submit = document.querySelector('input[type="submit"]')
+  submit.addEventListener('click', function(event){
+    addNewElementAsLi();
+    resetInput();
+  });
+}
+
+function clearEmployeeListOnLinkClick(){
+  let link = document.querySelector('a');
+  let ul = document.querySelector('ul');
+  link.addEventListener('click', function(event){
+    ul.innerHTML = '';
+  });
+}
+
+function resetInput(){
+  document.querySelector('input').value = '';
+}
