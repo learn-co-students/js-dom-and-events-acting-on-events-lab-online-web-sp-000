f6b47a1bb195fea3f52a873fafb5d331940faab9
jonmackeyparty <jon@jonmackey.party>
2019-05-29 16:42:45 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,33 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  $('ul.employee-list').append("<li>Bob</li>");
+}
+
+function addNewLiOnClick() {
+  let input = document.querySelector('input');
+  let submit = document.querySelector('input[type="submit"]');
+
+  submit.addEventListener('click', function(e) {
+    $('ul.employee-list').append("<li>Fred</li>");
+  })
+  input.value = "";
+}
+
+function clearEmployeeListOnLinkClick() {
+  let link = document.querySelector('a');
+  let list = document.querySelector('.employee-list');
+  let employees = document.querySelector('ul.employee-list li');
+
+  link.addEventListener('click', function(e) {
+    list.innerHTML = ""
+    }
+  );
+}
