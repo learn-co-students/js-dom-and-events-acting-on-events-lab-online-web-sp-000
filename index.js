cc0815d4c35ce06d5830b8f5d69ee2ec2da45461
alexmpalermo <alexmpalermo@gmail.com>
2019-06-05 16:06:39 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  let element = retrieveEmployeeInformation()
+  $('.employee-list').append(`<li>${element}</li>`);
+}
+
+function addNewLiOnClick() {
+  let input = document.querySelector('input')
+  let submit = document.querySelector('input[type="submit"]')
+
+  submit.addEventListener('click', function(event) {
+    addNewElementAsLi();
+    input.value = "";
+  });
+}
+
+function clearEmployeeListOnLinkClick() {
+  let ul = document.querySelector('ul')
+  let button = document.querySelector('a')
+
+  button.addEventListener('click', function(event) {
+    ul.innerHTML = "";
+  });
+}
