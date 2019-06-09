ceecd488ccc4d5aa0d636eb5c7967cb99daca57d
Btate712 <btate712@gmail.com>
2019-06-09 12:25:57 -0500

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,27 @@ function preventRefreshOnSubmit(){
     })
 }
 
+function retrieveEmployeeInformation() {
+  return $(':text').val();
+}
+
+function addNewElementAsLi() {
+  $('ul').append(`<li>${retrieveEmployeeInformation()}</li>`);
+}
+
+function addNewLiOnClick() {
+  let button = $(':submit:first')[0];
+  button.addEventListener('click', function(event) {
+    addNewElementAsLi();
+    $(':text').val("");
+  });
+}
+
+function clearEmployeeListOnLinkClick() {
+  let button = $('a:first')[0];
+  button.addEventListener('click', function(event) {
+    $('ul.employee-list').html('');
+  });
+}
+
 preventRefreshOnSubmit()
