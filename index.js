5839794bb47407bf5f1556f6e6b462c417d62c26
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-06-13 07:06:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
-    })
+      event.preventDefault();
+    });
 }
 
 preventRefreshOnSubmit()
+
+
+function retrieveEmployeeInformation(){
+  return document.querySelector('input').value;
+  // There are two "input"s in the html file. With no specificity, this refers to the first one.
+}
+
+
+function addNewElementAsLi() {
+  let employeeName = retrieveEmployeeInformation();
+  let ul = document.querySelector('.employee-list');
+  for (let i = 0; i < 1; i++) {
+    let li = document.createElement('li');
+    li.innerHTML = (i + 1).toString()+employeeName;
+    ul.appendChild(li);
+  }
+}
+
+
+function addNewLiOnClick() {
+  let submit = document.querySelector('input[type="submit"]');
+  submit.addEventListener('click', function(event) {addNewElementAsLi();
+  document.querySelector('input').value=''
+  })
+}
+
+
+function clearEmployeeListOnLinkClick() {
+  let link = document.querySelector('a');
+  let ul = document.querySelector('ul');
+  link.addEventListener('click', function(event) {
+    ul.innerHTML = ''
+  })
+}
