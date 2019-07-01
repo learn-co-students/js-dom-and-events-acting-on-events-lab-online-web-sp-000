468bab3e591f00ada478eb26f64eaf3aff5fd370
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-07-01 21:04:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 function preventRefreshOnSubmit(){
-    document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
-    })
+  document.querySelector('form').addEventListener('submit', function(event){
+    event.preventDefault()
+  })
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return $("input").val();
+}
+
+function addNewElementAsLi() {
+  const newLi = document.createElement('li');
+  newLi.innerHTML = retrieveEmployeeInformation();
+  $("ul.employee-list").append(newLi);
+}
+
+function addNewLiOnClick() {
+  addNewElementAsLi();
+  $("input").val("");
+}
+
+function clearEmployeeListOnLinkClick() {
+  $("a").click( function(e) {
+    $("ul.employee-list").empty();
+  });
+}
+
+// clearEmployeeListOnLinkClick(): This empties out the employee list when "Clear employee list" button clicked.
