57baf13d8fac97701e8386752299383c0e81d893
Jeff Benton <sikletz@gmail.com>
2019-07-25 19:56:12 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,26 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector("input").value;
+}
+
+function addNewElementAsLi() {
+  const li = document.createElement('li')
+  li.innerHTML = retrieveEmployeeInformation();
+  document.querySelector(".employee-list").appendChild(li);
+}
+
+function addNewLiOnClick() {
+  document.querySelector('input[type="submit"]').addEventListener('click', e => {
+    addNewElementAsLi();
+    document.querySelector("input").value = "";
+  });
+}
+
+function clearEmployeeListOnLinkClick() {
+  document.querySelector('a').addEventListener('click', e => {
+    document.querySelector('ul').innerHTML = '';
+  });
+}
