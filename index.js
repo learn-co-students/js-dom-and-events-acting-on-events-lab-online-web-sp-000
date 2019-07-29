b8ce42c12104e8ac35b5ebb4eded7b7034cbc9e9
blainelawson <blaine.lawson@gmail.com>
2019-07-29 14:31:15 -0400

    Done.

--- index.js
+++ index.js
@@ -6,21 +6,27 @@ function preventRefreshOnSubmit(){
 
 preventRefreshOnSubmit()
 
+let input = document.querySelector('input');
+
 function retrieveEmployeeInformation() {
     return document.querySelector('input').value;
 }
 
 function addNewElementAsLi() {
-    let ul = document.querySelector('ul.employee-list');
-    let li = document.createElement('li');
-    li.append(retrieveEmployeeInformation());
-    ul.appendChild(li);
+    // let ul = document.querySelector('ul.employee-list');
+    // let li = document.createElement('li');
+    // li.append(retrieveEmployeeInformation());
+    // ul.appendChild(li);
+
+    let employeeName = retrieveEmployeeInformation();
+    document.querySelector('ul.employee-list').insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`)
 }
 
 function addNewLiOnClick() {
   document.querySelector('input[type="submit"]').addEventListener('click', function(event) {
     addNewElementAsLi();
     document.querySelector('input').value = '';
+    // resetInput();
   })
 }
 
@@ -33,3 +39,7 @@ function clearEmployeeListOnLinkClick() {
       }
     }
   )};
+
+  // function resetInput(){
+  //   document.querySelector('input').value = ''
+  // }
