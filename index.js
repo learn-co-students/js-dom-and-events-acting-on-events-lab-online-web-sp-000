99fa311d8450c156a2c03196e4b2db41b3dbe944
github username <github email address>
2019-08-15 10:30:51 -0400

    Done.

--- index.js
+++ index.js
@@ -3,5 +3,37 @@ function preventRefreshOnSubmit(){
       event.preventDefault()
     })
 }
-
 preventRefreshOnSubmit()
+
+const getInput = document.getElementById("get-this")
+const getUl = document.querySelector(".employee-list")
+
+function retrieveEmployeeInformation(){
+  return getInput.value
+}
+
+function addNewElementAsLi(){
+  let nameValue = retrieveEmployeeInformation()
+  const newLi = document.createElement("li")
+  newLi.innerText = nameValue
+  getUl.appendChild(newLi)
+}
+
+function addNewLiOnClick(){
+  let submit = document.querySelector('input[type="submit"]')
+    submit.addEventListener('click', function(event){
+      addNewElementAsLi()
+      resetInputValue()
+    })
+  }
+
+function resetInputValue(){
+  document.querySelector('input').value = ''
+}
+
+function clearEmployeeListOnLinkClick(){
+  const getATag = document.querySelector("a")
+  getATag.addEventListener("click", function(){
+    getUl.innerHTML = ""
+  })
+}
