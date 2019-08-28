d401a4a70be9bdb2d7096ddaf1f8c8562bfc61f1
Rachel <williamsrachelada@gmail.com>
2019-08-28 15:58:26 -0700

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input[type=text]').value
+}
+
+function addNewElementAsLi() {
+  let ul = document.querySelector('.employee-list');
+  let li = document.createElement('li');
+  li.innerHTML = retrieveEmployeeInformation().toString();
+  ul.append(li);
+}
+
+function addNewLiOnClick() {
+  const input = document.querySelector('input[type=submit]');
+
+  input.addEventListener('click', function(e) {
+    addNewElementAsLi();
+    document.querySelector('input[type=text]').value = '';
+  });
+}
+
+function clearEmployeeListOnLinkClick() {
+  const link = document.querySelector('a');
+
+  link.addEventListener('click', function(e) {
+    document.querySelector('.employee-list').innerHTML = '';
+  });
+}
