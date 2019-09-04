b7a50d9affca1473fdaf473bb88da594d3137186
Ella <michaella.latkovic@gmail.com>
2019-09-03 17:05:22 -0700

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,42 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+ let input = document.querySelector('input').value;
+ return input;
+}
+
+function addNewElementAsLi() {
+  let li = document.createElement('li');
+  let ul = document.querySelector('.employee-list');
+
+  li.innerHTML = retrieveEmployeeInformation();
+  ul.append(li);
+}
+
+function clearInputValue(){
+ document.querySelector('input').value = '';
+}
+
+function addNewLiOnClick() {
+  let action = document.querySelector('input[type="submit"]');
+
+  action.addEventListener('click', function(e){
+    addNewElementAsLi()
+    clearInputValue()
+  });
+}
+
+function clearEmployeeList(){
+  let ul = document.querySelector('.employee-list');
+  ul.innerHTML = '';
+}
+
+function clearEmployeeListOnLinkClick() {
+  let action = document.querySelector('a');
+
+  action.addEventListener('click', function(e){
+    clearEmployeeList()
+  });
+}
