2c8c29e1baa2a6012787ba3bd3b9efa101e4982b
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-09-14 00:30:28 +0100

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  return document.querySelector('.employee-list').append(`<li>${retrieveEmployeeInformation()}</li>`);
+}
+
+function addNewLiOnClick() {
+  let submit = document.querySelector('input[type="submit"]');
+
+  submit.addEventListener('click', function(e) {
+    addNewElementAsLi();
+    document.querySelector('input').value = '';
+  });
+}
+
+function clearEmployeeListOnLinkClick() {
+  let ul = document.querySelector('ul');
+  let button = document.querySelector('a');
+
+  button.addEventListener('click', function(e) {
+    ul.innerHTML = '';
+  });
+}
