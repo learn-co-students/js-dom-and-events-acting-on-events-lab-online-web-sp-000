bed66cd62404b18c16fa2b90b657fb650669175c
Andrew Davisson <akdavisson4@gmail.com>
2019-09-18 23:00:34 -0600

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-function preventRefreshOnSubmit(){
-    document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
-    })
+function preventRefreshOnSubmit() {
+  document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+  });
 }
 
-preventRefreshOnSubmit()
+preventRefreshOnSubmit();
+
+function retrieveEmployeeInformation() {
+  const inputs = $("input");
+  return inputs[0].value;
+}
+
+function addNewElementAsLi() {
+  const list = $(".employee-list");
+  list.append(`<li>${retrieveEmployeeInformation()}</li>`);
+}
+
+function addNewLiOnClick() {
+  const inputs = $("input");
+  inputs[1].addEventListener("click", addNewElementAsLi);
+  inputs[1].addEventListener("click", function(e) {
+    inputs[0].value = "";
+  });
+}
+
+function clearEmployeeListOnLinkClick() {
+  const list = $(".employee-list");
+  const link = $('a[href="#"]')[0];
+  
+  link.addEventListener('click', function(e){
+    list.empty();
+  });
+}
