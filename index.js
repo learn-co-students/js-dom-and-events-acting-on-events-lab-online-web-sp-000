6311335a8c80527e615e4308a00b7fcb5338fe44
tan-nate <nktan93@gmail.com>
2019-09-23 10:01:00 -0700

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi() {
+  let employee_list = document.querySelector('ul.employee-list')
+  let li = document.createElement('li')
+  li.innerHTML = retrieveEmployeeInformation()
+  employee_list.appendChild(li)
+}
+
+function addNewLiOnClick() {
+  let button = document.querySelector('input[type="submit"]')
+  let text_field = document.querySelector('input[type="text"]')
+  button.addEventListener('click', function(event) {
+    addNewElementAsLi()
+    text_field.value = ''
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  let link = document.querySelector('a')
+  link.addEventListener('click', function(event) {
+    let employee_list = document.querySelector('ul.employee-list')
+    employee_list.innerHTML = ''
+  })
+}
