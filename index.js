1eb9cbf12e44d505128d6280799741d651599dd6
elijahmm17 <elijahmm17@gmail.com>
2019-10-01 22:22:09 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,36 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  let list = document.querySelector('ul.employee-list');
+  let li = document.createElement('li');
+  li.innerHTML = retrieveEmployeeInformation();
+  
+  list.appendChild(li);
+}
+
+function addNewLiOnClick() {
+  let input = document.querySelector('input');
+  let s = document.querySelector('input[type=submit]');
+  
+  s.addEventListener('click', function() {
+    addNewElementAsLi();
+    input.value = '';
+  });
+}
+
+function clearEmployeeListOnLinkClick() {
+  let list = document.querySelector('ul.employee-list');
+  let clearListButton = document.querySelector('a');
+  
+  clearListButton.addEventListener('click', function() {
+    list.innerHTML = '';
+  });
+}
