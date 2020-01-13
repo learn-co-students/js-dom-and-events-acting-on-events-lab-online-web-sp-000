ea8b1310a1d585b1f61d73523ccce5821a17b643
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-01-13 04:15:42 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,32 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let input = document.querySelector('input')
+
+function retrieveEmployeeInformation() {
+  return input.value
+}
+
+function addNewElementAsLi() {
+  let employee = retrieveEmployeeInformation()
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employee}</li>`)
+}
+
+function addNewLiOnClick() {
+  // let employeeName = addNewElementAsLi()
+  let submit = document.querySelector('input[type="submit"]')
+  submit.addEventListener('click', function(event){
+  addNewElementAsLi()
+  input.value = ""
+  })
+
+
+}
+function clearEmployeeListOnLinkClick() {
+  let link = document.querySelector('a')
+  let ul = document.querySelector('ul')
+  link.addEventListener('click', function(event){
+     ul.innerHTML = ""
+  })
+}
